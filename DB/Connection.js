3513c3b325a26f79e3465af91c37c225dc9f77aa
degod <degodtest@gmail.com>
2020-07-31 12:56:38 +0100

    added cors

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = "";
+const URI = "mongodb+srv://fmj:Phemmyladey106@cluster0.9zh0n.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
 const connectDB = async() => {
 	await mongoose.connect(URI, { 
 		useUnifiedTopology: true, 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const connectDB = require('./DB/Connection');
+const cors = require('cors');
 const app = express();
 
 connectDB();
+app.use(cors({origin: '*'}));
 app.use(express.json({ extended:false }));
 app.use('/api', require('./Api/Certificate'));
 const Port = process.env.Port || 3000;
